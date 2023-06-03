8831cf0386a4b1e59dfbf71af68b114bd80aff78
NatanEnggalSwasono <natan.e2042@student.ukrimuniversity.ac.id>
2023-06-04 04:02:19 +0700

    mengupload semua file ge github

---
+++ src/scripts/index.js
@@ -0,0 +1,30 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import swRegister from './utils/sw-register';
+import App from './views/app';
+
+const app = new App({
+  menuHamburger: document.querySelector('#hamburger'),
+  navList: document.querySelector('.nav-list'),
+  mainElement: document.querySelector('.nav-list'),
+  content: document.querySelector('#resto-content'),
+});
+
+const skipLink = document.querySelector('.skip-link');
+const mainContent = document.querySelector('#maincontent');
+
+// tutorial dari reviewer
+skipLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  mainContent.scrollIntoView({ behavior: 'smooth' });
+  skipLink.blur();
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
+  swRegister();
+});
